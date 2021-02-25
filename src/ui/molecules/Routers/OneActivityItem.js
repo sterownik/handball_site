9c375018ca89fc59c20e7fd24d9e622af2267702
sterownik <4golda4@gmail.com>
2021-02-25 14:34:55 +0100

    css fixes.

--- src/ui/molecules/Routers/OneActivityItem.js
+++ src/ui/molecules/Routers/OneActivityItem.js
@@ -7,14 +7,64 @@ const OneActivityItem = () => {
     const activityData = useSelector(controlActivityData);
 
     return (
-        <StyledWrapper>{activityData.activity.id}</StyledWrapper>
+        <StyledWrapper>
+            <StyledImg src={activityData.activity.imgSource} alt=""/>
+            <StyledTitle>{activityData.activity.title}</StyledTitle>
+            <StyledText>{activityData.activity.text}</StyledText>
+        </StyledWrapper>
     )
 };
 
+const StyledTitle = styled.h2`
+    padding: 20px;
+    font-family: cursive;
+    
+    @media (max-width: 720px) {
+        text-align: center;
+    }
+`;
+
+const StyledText = styled.p`
+    font-size: 20px;
+    padding: 10px;
+    text-align: justify;
+`;
+
 const StyledWrapper = styled.div`
-    background-color: red;
-    width: 70%;
+    width: 60%;
     margin: 3% auto;
+    display: flex-inline;
+    position: relative;
+    
+    @media (max-width: 720px) {
+        width: 90%;
+        padding-top: 20px;
+    }
+    
+    &:after {
+        content: "";
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: ${(props) => props.theme.colorGreenLight};
+        top: 0;
+        z-index: -1;
+        border-radius: 15px;
+    }
+`;
+
+const StyledImg = styled.img`
+    float: left;
+    margin: 20px;
+    width: 55%;
+    box-shadow: 0 10px 10px -5px;
+    
+    @media (max-width: 720px) {
+        width: 80%;
+        float: none;
+        margin: 0 auto;
+        display: block;
+    }
 `;
 
 export default OneActivityItem;
